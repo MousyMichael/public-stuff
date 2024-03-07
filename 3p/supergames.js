cd28a1312464c03fdb3c07ebe4bf0e9344efc3c5
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-06 20:06:44 -0500

    Update supergames.js

--- 3p/supergames.js
+++ 3p/supergames.js
@@ -1,1 +1,11 @@
-//For SuperGames 
+window.onload = (e) => {
+  var cache {
+    url:window.location.href,
+    host:window.location.hostname,
+    sessionId:sessionId()
+  };
+  var allE = document.querySelectorAll("*");
+  allE.forEach((e) => {
+    
+  });
+}
