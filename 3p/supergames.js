241e82fb2563808eee8ee72dfe07fb6a6a70eb70
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-07 16:58:44 -0500

    Update supergames.js

--- 3p/supergames.js
+++ 3p/supergames.js
@@ -1,7 +1,7 @@
 window.onload = () => {
 var ah = document.querySelector(".easter-egg-imp");
   ah.innerHTML = 'Patches, additional features, and bug fixes by MousyM';
-  
+window.alert("Hey everyone, games now remain hidden from GoGuardian when launched from a hidden window!")
   const allGameButtons = document.querySelectorAll("a"); 
   allGameButtons.forEach((b) => {
     if(b.getAttribute("target") == "_blank") {
