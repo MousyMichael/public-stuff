97696ac40fe3b67e36648f7a820b1666558f8a09
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-07 16:48:33 -0500

    Update supergames.js

--- 3p/supergames.js
+++ 3p/supergames.js
@@ -1,4 +1,8 @@
 window.onload = () => {
+var style = document.createElement("style");
+  style.innerHTML = '.easter-egg-imp {color:lime!important;}';
+var docBody = document.querySelector("body"); 
+  docbody.appendChild(style)
 var ah = document.querySelector(".easter-egg-imp");
   ah.innerHTML = 'Patches, additional features, and bug fixes by MousyM';
   
