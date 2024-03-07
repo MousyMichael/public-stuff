d43d669cee2a55e8a7ca1b2d19435dedbbd7a7f5
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-07 16:53:51 -0500

    Update supergames.js

--- 3p/supergames.js
+++ 3p/supergames.js
@@ -2,7 +2,7 @@ window.onload = () => {
 var ah = document.querySelector(".easter-egg-imp");
   ah.innerHTML = 'Patches, additional features, and bug fixes by MousyM';
   
-  const allGameButtons = doucment.querySelectorAll("a"); 
+  const allGameButtons = document.querySelectorAll("a"); 
   allGameButtons.forEach((b) => {
     if(b.getAttribute("target") == "_blank") {
       b.removeAttribute("target"); 
