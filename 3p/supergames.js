278824379e09a7bfa2e7f5a58779216afe171858
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-06 20:36:28 -0500

    Update supergames.js

--- 3p/supergames.js
+++ 3p/supergames.js
@@ -6,19 +6,17 @@ window.onload = (e) => {
   
   var linkStyles = document.createElement("link"); 
   linkStyles.rel = 'stylesheet';
-  linkStyles.href = 'https://x.unblockedsearch.xyz/3p/styles.css';
+  linkStyles.href = 'https://x.ube.lol/3p/styles.css';
   window.document.head.appendChild(linkStyles)
   
   var ah = document.querySelector(".easter-egg-imp");
-  //Fix for error in code
-  ah.removeAttribute("onclick"); 
-  
   ah.addEventListener("click", (e) => {
     var popup = document.createElement("div"); 
     popup.classList = 'sge-popup';
     popup.innerHTML = '<div class="sge-inner-popup"><textarea placeholder="Enter HTML code to open in ab window"></textarea><button id="sge-launch">Open</button><button id="sge-close">Close</button></div>';
     
     window.document.body.appendChild(popup)
+    
 
   });
 }
