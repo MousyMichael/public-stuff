84efd3d900e33f64a6a840931e9c3ad80c87d235
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-06 21:31:34 -0500

    Update supergames.js

--- 3p/supergames.js
+++ 3p/supergames.js
@@ -5,10 +5,4 @@ var ah = document.querySelector(".easter-egg-imp");
     alert("wow. you found this.")
     ah.innerHTML = 'Did you know unblocked search originally had over 10 functions just to open a hidden windows? - MousyM';
   });
-var all = document.querySelectorAll("span,a,button,p,div");
-  all.forEach((e) => {
-    e.addEventListener("click", (ev) => {
-      ev.style.outline = 'lime solid 2px !important';
-    });
-  });
 }
