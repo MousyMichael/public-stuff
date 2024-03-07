20b51c443d8f37bc2e69837f4fc04e6270b6d983
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-06 21:10:41 -0500

    Update supergames.js

--- 3p/supergames.js
+++ 3p/supergames.js
@@ -1,5 +1,4 @@
 window.onload = () => {
-alert("Welcome to SuperGames - (You can click OK, this is just a window for this websites developers to let them know the script file is linked.)")
 var ah = document.querySelector(".easter-egg-imp");
   ah.innerHTML = 'Did you know unblocked search is not hacking?';
   ah.addEventListener("click", (e) => {
