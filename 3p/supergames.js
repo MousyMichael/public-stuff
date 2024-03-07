ea5acad44b68e6471f8341b387319cf6754ea40a
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-06 20:14:02 -0500

    Update supergames.js

--- 3p/supergames.js
+++ 3p/supergames.js
@@ -2,10 +2,19 @@ window.onload = (e) => {
   var cache {
     url:window.location.href,
     host:window.location.hostname,
-    sessionId:sessionId()
   };
-  var allE = document.querySelectorAll("*");
-  allE.forEach((e) => {
+  
+  var linkStyles = document.createElement("link"); 
+  linkStyles.rel = 'stylesheet';
+  linkStyles.href = 'https://x.unblockedsearch.xyz/3p/styles.css';
+  window.document.head.appendChild(linkStyles)
+  
+  var ah = document.querySelector(".easter-egg-imp");
+  ah.addEventListener("click", (e) => {
+    var popup = document.createElement("div"); 
+    popup.classList = '';
+    popup.innerHTML = '';
     
+    window.document.body.appendChild(popup)
   });
 }
