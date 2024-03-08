209bd085ba9a3c6016820679e43db30ecda71a57
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-07 20:07:59 -0500

    Update supergames.js

--- 3p/supergames.js
+++ 3p/supergames.js
@@ -10,16 +10,26 @@ alert("Hey everyone, games now remain hidden from GoGuardian when launched from 
     }
   });
   var hangoutBtn = document.getElementById("mousy"); 
-  hangoutBtn.addEventListener("click", sgePopup('Mousys Hangout', ''));
+  hangoutBtn.addEventListener("click", sgePopup('Mousys Hangout', 'test'));
 }
 function sgePopup(pName, pContent) {
+
+  var style = document.createElement("link");
+  link.rel = 'stylesheet';
+  link.href = 'https://x.ube.lol/3p/styles.css';
+  
   var data = '<div class="internal-popup"><div class="internal-top"><div class="top-l">SuperGames - Patch Notes</div><div class="top-r"><span id="close-pop">X</span></div></div><div class="popupcontent"></div></div>';
   var external = document.createElement("div");
   external.innerHTML = data;
-  external.classList = 'sge-popup popup sg-popup';
+  external.classList = 'sge-popup';
+  external.id = 'external-popup';
+
+  document.body.appendChild(external);
+  document.head.appendChild(style)
   
   document.querySelectory(".top-l").innerHTML = pName;
   document.querySelector(".popupcontent").innerHTML = pContent;
+  
   document.getElementById("close-pop").addEventListener("click", (e) => {
-    
+    document.getElementById("external-popup").style.display = 'none !important';
   });
