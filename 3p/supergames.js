0153ad3d61ad09f063f66aba498a0d7aa7470349
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-07 20:04:17 -0500

    Update supergames.js

--- 3p/supergames.js
+++ 3p/supergames.js
@@ -10,8 +10,16 @@ alert("Hey everyone, games now remain hidden from GoGuardian when launched from 
     }
   });
   var hangoutBtn = document.getElementById("mousy"); 
-  hangoutBtn.addEventListener("click", sgePopup("Mousy's Hangout", ""));
+  hangoutBtn.addEventListener("click", sgePopup('Mousys Hangout', ''));
 }
-function sgePopup() {
+function sgePopup(pName, pContent) {
+  var data = '<div class="internal-popup"><div class="internal-top"><div class="top-l">SuperGames - Patch Notes</div><div class="top-r"><span id="close-pop">X</span></div></div><div class="popupcontent"></div></div>';
+  var external = document.createElement("div");
+  external.innerHTML = data;
+  external.classList = 'sge-popup popup sg-popup';
   
-}
+  document.querySelectory(".top-l").innerHTML = pName;
+  document.querySelector(".popupcontent").innerHTML = pContent;
+  document.getElementById("close-pop").addEventListener("click", (e) => {
+    
+  });
