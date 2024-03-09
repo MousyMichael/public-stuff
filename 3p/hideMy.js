7ae325a1a08ffff17ea9f67437c7e19c2527f14b
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-09 10:40:08 -0500

    Create hideMy.js

---
+++ 3p/hideMy.js
@@ -0,0 +1,38 @@
+function openHiddenWindow(url) {
+	var hWin = window.open("",""); 
+  
+  if(hWin) {
+  	var header = document.createElement("div");
+    header.innerHTML = 'This tab is hidden from GoGuardian so your teacher cannot see this page!<span style="font-weight:bold;font-size:14px;color:gray;text-shadow:none;">HideMyWindow Developed By: MousyM</span>';
+   	hs = header.style;
+    hs.display = 'flex';
+    hs.flexDirection = 'column';
+    hs.justifyContent = 'center';
+    hs.alignItems = 'center'
+    hs.fontSize = '18px';
+    hs.width = '100%';
+    hs.color = 'lime';
+    hs.fontFamily = 'arial';
+    hs.textShadow = '0 0 15px lime';
+    hs.backgroundColor = '#161616';
+    hs.height = '45px';
+    
+  	var contentFrame = document.createElement("iframe")
+    contentFrame.src = url;
+    cfs = contentFrame.style; 
+    cfs.width = '100%';
+    cfs.height = 'calc(100% - 45px)';
+    cfs.border = 'none';
+    
+    hws = hWin.document.body.style;
+    hws.margin = '0px';
+    hws.height = '100vh';
+    hws.width = '100vw';
+    
+    hWin.document.body.appendChild(header)
+    hWin.document.body.appendChild(contentFrame)
+  } else {
+  	alert("Oh no! Something went wrong and we can't cloak this window at the moment, please try again later!")
+  }
+
+}
