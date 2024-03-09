c69f3411543e2a34e2016e4485dfe28348d2c4e0
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-09 09:57:03 -0500

    Create simpleSearch.js

---
+++ 3p/simpleSearch.js
@@ -0,0 +1,26 @@
+document.getElementById("search-bar").addEventListener("input", (ev) => {
+	if(document.getElementById("search-bar").value < 0) {
+  	document.querySelectorAll(".button h2").forEach((bh) => {
+    	bh.parentElement.parentElement.classList.remove("hidden");
+    });
+  } else {
+  	searchElements();
+  }
+});
+
+function searchElements() {
+var allE = document.querySelectorAll(".button h2");
+var query = document.getElementById("search-bar").value;
+allE.forEach((ae) => {
+	if(ae.textContent.toLowerCase().includes(query) == true) {
+  	if(ae.parentElement.parentElement.classList.contains("hidden") == true) {
+    	ae.parentElement.parentElement.classList.remove("hidden")
+    } 
+  } else {
+  	if(ae.parentElement.parentElement.classList.contains("hidden") !== true) {
+    	ae.parentElement.parentElement.classList.add("hidden");
+    }
+  }
+});
+
+} 
