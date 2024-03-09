eed77f33269a0d0ec39c042c4f82c3f19909984a
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-09 11:09:11 -0500

    Update supergames.js

--- 3p/supergames.js
+++ 3p/supergames.js
@@ -8,26 +8,3 @@ all.forEach((aa) => {
   }
 });
 }
-
-//Popup testing
-function sgePopup(pName, pContent) {
-
-  var style = document.createElement("link");
-  link.rel = 'stylesheet';
-  link.href = 'https://x.ube.lol/3p/styles.css';
-  
-  var data = '<div class="internal-popup"><div class="internal-top"><div class="top-l">SuperGames - Patch Notes</div><div class="top-r"><span id="close-pop">X</span></div></div><div class="popupcontent"></div></div>';
-  var external = document.createElement("div");
-  external.innerHTML = data;
-  external.classList = 'sge-popup';
-  external.id = 'external-popup';
-
-  document.body.appendChild(external);
-  document.head.appendChild(style);
-  
-  document.querySelectory(".top-l").innerHTML = pName;
-  document.querySelector(".popupcontent").innerHTML = pContent;
-  
-  document.getElementById("close-pop").addEventListener("click", (e) => {
-    document.getElementById("external-popup").style.display = 'none !important';
-  });
