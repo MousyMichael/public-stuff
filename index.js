38080c4209e2e43f2105470eb0f078e1e7b11734
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-09 10:42:07 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+<html>
+  <head>
+  <script src="https://x.ube.lol/3p/hideMy.js"></script>
+  </head>
+  <body>
+<button onclick="openHiddenWindow('https://ubg.servegame.com')" id="id">Click to open hidden window</button>
+  </body>
+  </html>
