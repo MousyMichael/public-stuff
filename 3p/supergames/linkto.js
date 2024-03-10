4d92d69805b7637221faef2b9b408461f3421e6b
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-03-09 22:15:31 -0500

    Update linkto.js

--- 3p/supergames/linkto.js
+++ 3p/supergames/linkto.js
@@ -3,3 +3,7 @@ const globalConfiguration = {};
 
 import sSConf from './simpleSearch.js';
 import {searchElements, windowLoad} from './simpleSearch.js';
+
+window.onload = () => {
+  //List load functions for each module
+}
